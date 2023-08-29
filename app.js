95779a08dce464f19161ab0b41df35aad1082cdb
Deepshri M <deepshrim613@gmail.com>
2023-08-29 22:02:23 +0530

    FindByid query

--- app.js
+++ app.js
@@ -29,9 +29,24 @@ bookRouter.route('/books').get((req, res) => {
     //   }
 
 	//http://localhost:4000/api/books?genre=Fantasy
-	const {query}=req;
-	
+	const query={}
+
+	if(req.query.genre){
+		query.genre=req.query.genre
+	}
+
 	Book.find(query).exec() 
+	.then(docs=>res.status(200)
+	.json(docs))
+	.catch(err=>res.status(500)
+	.json({
+		message:"Error to fetch",
+			error:err }))
+});
+
+bookRouter.route('/books/:id').get((req, res) => {
+
+	Book.findById(req.params.id).exec() 
 	.then(docs=>res.status(200)
 	.json(docs))
 	.catch(err=>res.status(500)
